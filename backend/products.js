392666c76805c9184467b03a50a28b9a799eea42
Attoumidriss2001 <attoumi2001@gmail.com>
2023-07-09 02:12:38 +0100

    first commit

---
+++ backend/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink: [
+    "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860756/online%20store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_v7kymo.png",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860731/online%20store/1/NASA_Space_Bear_Print_T-Shirt_bpdpcj.jpg",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860727/online%20store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_q1ujvg.jpg",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860725/online%20store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_1_tg6fkn.jpg"
+    ],
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: [
+      "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860762/online%20store/2/16480232420809ac54cac58a758d88fbee070f7f7a_thumbnail_900x_qvsbhj.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860756/online%20store/2/1658329192737168ff6ad063452563108e1de4d132_thumbnail_900x_lhxuym.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860756/online%20store/2/1646271078e49bf65969984162cd89e52f225c1251_thumbnail_900x_dtlitb.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860755/online%20store/2/1648023235ba32327d7aecda5b9de9913efa316d9e_thumbnail_900x_e56oea.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860755/online%20store/2/164802324294a9e00cd18e3bdacdb07faadc6ea08e_thumbnail_900x_ahc1el.webp"
+    ],
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink: [
+    "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860727/online%20store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Khaki___XL_mlrdqv.png",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860727/online%20store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_White___XL_kb17bv.png",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860726/online%20store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Grey___XL_rfuulw.png",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860706/online%20store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Black___XL_k9cqsc.png"
+    ],
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: [
+     "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860744/online%20store/4/1646880634f3ecf23038717f7adbcf3658effab2e7_thumbnail_900x_ol4r98.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860723/online%20store/4/1646822799c97e5f9754d3b3314b4e6ffd848ad22d_thumbnail_900x_bfagmq.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860722/online%20store/4/164430580182db3b5a5192e660a87c832a397044b6_thumbnail_900x_qmt0cn.webp",
+        "https://res.cloudinary.com/dbsv1lrhj/image/upload/v1688860721/online%20store/4/163826656927114e9da27ea115f55404141ba11264_thumbnail_900x_hzppt9.webp"
+    ],
+  },
+];
+module.exports = products;
